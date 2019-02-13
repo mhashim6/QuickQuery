b700bd1ed505da13a217a54dda819666f36fb799
mhashim6 <m6hashim@gmail.com>
2019-02-13 12:17:07 +0200

    main coroutine scope to assure showing dialog in main thread.

--- app/src/main/java/mhashim6/android/quickQuery/ClipboardMonitor.kt
+++ app/src/main/java/mhashim6/android/quickQuery/ClipboardMonitor.kt
@@ -14,6 +14,7 @@ import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
+import kotlinx.coroutines.launch
 import java.net.MalformedURLException
 import java.net.URL
 
@@ -62,7 +63,9 @@ class ClipboardMonitor : Service() {
                 URL(it.toString()) //ignore if url.
             } catch (e: MalformedURLException) {
                 this.query = it
-                showNewBubble()
+                mainScope.launch {
+                    showNewBubble()
+                }
             }
         }
     }
--- app/src/main/java/mhashim6/android/quickQuery/Utils.kt
+++ app/src/main/java/mhashim6/android/quickQuery/Utils.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 /**
  * Created by mhashim6 on 29/10/2017.
@@ -14,6 +16,8 @@ const val GOOGLE_PLAY_LINK_PRO = "market://details?id=mhashim6.android.quickQuer
 const val DONATE_LINK = "http://paypal.com/paypalme/mhashim6"
 const val COPY_KEY = "copy_key"
 val IS_OREO = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+
+val mainScope = CoroutineScope(Dispatchers.Main)
 
 fun openWebPage(context: Context, url: String) {
     val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
